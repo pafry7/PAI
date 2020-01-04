482d4bd46bf62aed660bb89c57d759edd3846152
Patryk Fryda <patryk.fryda@kolomololabs.com>
2020-01-04 16:46:02 +0100

    refactor: change server from koa to express

--- budgetology-server/src/index.ts
+++ budgetology-server/src/index.ts
@@ -1,21 +1,41 @@
 import "reflect-metadata";
 
-import { ApolloServer } from "apollo-server-koa";
-import BodyParser from "koa-bodyparser";
-import { HelloWorldResolver } from "./resolvers/HelloWorldResolver";
-import Koa from "koa";
+import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
+import express from "express";
+import session from "express-session";
+
+const connectSqlite3 = require("connect-sqlite3");
+
+const SQLiteStore = connectSqlite3(session);
 
 (async () => {
-  const app = new Koa();
-  app.use(BodyParser());
+  const app = express();
+
+  app.use(
+    session({
+      store: new SQLiteStore({
+        db: "database.sqlite",
+        concurrentDB: true
+      }),
+      name: "qid",
+      secret: process.env.SESSION_SECRET || "aslkdfjoiq12312",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365 // 7 years
+      }
+    })
+  );
 
   await createConnection();
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloWorldResolver],
+      resolvers: [__dirname + "/resolvers/**/*.ts"],
       validate: true,
       emitSchemaFile: true
     }),
