115d45a0b7b5a363dd57cc16f9d1fc7dfc961665
Patryk Fryda <patryk.fryda@kolomololabs.com>
2020-01-24 14:12:42 +0100

    feat: add queries to BankAccountResolver

--- budgetology-server/src/resolvers/BankAccountResolver.ts
+++ budgetology-server/src/resolvers/BankAccountResolver.ts
@@ -1,8 +1,9 @@
-import { Resolver, Mutation, Arg, UseMiddleware } from "type-graphql";
+import { Resolver, Mutation, Arg, UseMiddleware, Query } from "type-graphql";
 import { User } from "entity/User";
 import { isAuth } from "middleware/isAuth";
 import { BankAccountInput } from "graphql-types/BankAccountInput";
 import { BankAccount } from "entity/BankAccount";
+import { Expense } from "entity/Expense";
 
 @Resolver()
 export class BankAccountResolver {
@@ -19,5 +20,43 @@ export class BankAccountResolver {
       user: user
     }).save();
     return true;
+  }
+
+  @Query(() => [Expense])
+  @UseMiddleware(isAuth)
+  async expenses(@Arg("id") id: string): Promise<Expense[] | undefined> {
+    const bankAccount = await BankAccount.findOne(id, {
+      relations: ["expenses"]
+    });
+    if (!bankAccount) {
+      return undefined;
+    }
+    return bankAccount.expenses;
+  }
+
+  @Query(() => BankAccount)
+  @UseMiddleware(isAuth)
+  async bankAccount(@Arg("id") id: string): Promise<BankAccount | undefined> {
+    const bankAccount = await BankAccount.findOne(id, {
+      relations: ["expenses"]
+    });
+    if (!bankAccount) {
+      return undefined;
+    }
+    return bankAccount;
+  }
+
+  @Query(() => [BankAccount])
+  @UseMiddleware(isAuth)
+  async bankAccounts(
+    @Arg("id") id: string
+  ): Promise<BankAccount[] | undefined> {
+    const user = await User.findOne(id, {
+      relations: ["bankAccounts", "bankAccounts.expenses"]
+    });
+    if (!user) {
+      return undefined;
+    }
+    return user.bankAccounts;
   }
 }
